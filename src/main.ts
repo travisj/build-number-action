d642110585bbbff3bf2cde2ff97c823b9b4f618c
Travis Johnson <travis@jumprope.com>
2019-12-21 15:39:24 -0500

    passing github context

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import * as core from '@actions/core'
 import axios from 'axios'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
     const key: string = core.getInput('key')
     const url = `https://build-numbers.herokuapp.com/${key}`
-    const response = await axios.get(url)
+    const response = await axios.post(url, github.context, {
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
     const buildNumber = response.data
     core.setOutput('build-number', buildNumber)
   } catch (error) {
